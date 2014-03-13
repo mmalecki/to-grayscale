53ecaa0e840219b7b11b00189d88059355039545
Maciej Małecki <me@mmalecki.com>
2014-03-13 18:41:13 +0100

    Add a comment about origin of coefficients

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ module.exports = function (rgb, options) {
   var depth = opts.depth || 24;
   var size = rgb.length / (depth / 8);
 
+  // Default values for those coefficients are stolen from
+  // http://stackoverflow.com/q/687261, althought they are mostly experimental
+  // and fine-tunable.
+  // http://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
+  // is an example of different set of values.
   var coefficients = opts.coefficients || {};
   coefficients.r = coefficients.r || 0.299;
   coefficients.g = coefficients.g || 0.587;
